9e772b2f1e4ed4ec6c31e1114268a08be981ff84
EugenioGiron <eugeniog_1994@hotmail.com>
2026-05-19 16:22:23 -0600

    fix cors and raiway

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,7 +9,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api/v1');
-  app.enableCors({ origin: '*', methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS' });
+  app.enableCors({
+  origin: [
+    'https://microfin-chi.vercel.app',
+    'http://localhost:4200'
+  ],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+});
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: false, transform: true }));
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new TransformInterceptor());
@@ -25,7 +33,7 @@ async function bootstrap() {
   SwaggerModule.setup('api/v1/docs', app, document);
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`🚀 MicroFin API corriendo en http://localhost:${port}/api/v1`);
   console.log(`📖 Swagger en http://localhost:${port}/api/v1/docs`);
 }
