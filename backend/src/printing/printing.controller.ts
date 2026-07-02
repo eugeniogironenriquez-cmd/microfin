035bb8950734d3f037e7d8d6ff9298049f87493e
EugenioGiron <eugeniog_1994@hotmail.com>
2026-07-02 11:37:03 -0600

    Cambios

--- backend/src/printing/printing.controller.ts
+++ backend/src/printing/printing.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Param, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { PaymentsService } from '../payments/payments.module';
+import { Auth, AuthPermission, CurrentUser } from '../common/guards/roles.guard';
 
 @Controller('printing')
 export class PrintingController {
@@ -8,7 +9,7 @@ export class PrintingController {
     private readonly paymentsService: PaymentsService,
   ) {}
 
-  @Get('ticket/:id/html')
+  @Get('ticket/:id/html') @Auth()
   async ticketHtml(
     @Param('id') id: string,
     @Res() res: Response,
